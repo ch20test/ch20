d35b67cdabc0a6ad9ca0bcc93ab809962f8c406e
huxinbei <2568410109@qq.com>
2020-12-03 15:43:10 +0800

    huxinbei

--- src/main/java/com/controller/admin/AdminOrderController.java
+++ src/main/java/com/controller/admin/AdminOrderController.java
@@ -5,19 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import com.service.admin.AdminOrderService;
+import com.service.admin.AdminOrderService;
 
-//@Controller
-//@RequestMapping("/adminOrder")
-//public class AdminOrderController extends BaseController{
-//	@Autowired
-//	private AdminOrderService adminOrderService;
-//	@RequestMapping("/orderInfo")
-//	public String orderInfo(Model model) {
-//		return adminOrderService.orderInfo(model);
-//	}
-//	@RequestMapping("/deleteorderManager")
-//	public String deleteorderManager(Integer id) {
-//		return adminOrderService.deleteorderManager(id);
-//	}
-//}
+@Controller
+@RequestMapping("/adminOrder")
+public class AdminOrderController extends BaseController{
+	@Autowired
+	private AdminOrderService adminOrderService;
+	@RequestMapping("/orderInfo")
+	public String orderInfo(Model model) {
+		return adminOrderService.orderInfo(model);
+	}
+	@RequestMapping("/deleteorderManager")
+	public String deleteorderManager(Integer id) {
+		return adminOrderService.deleteorderManager(id);
+	}
+}
